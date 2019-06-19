3741141708875a5a13068d298f0d31435ce54c87
Robert Plata <norbertedguy@gmail.com>
2019-06-19 14:03:08 -0400

    Update ClientHolder.js

--- ClientHolder.js
+++ ClientHolder.js
@@ -1,9 +1,11 @@
 class ClientHolder {
+
     TwitchClient = require('twitch').default;
     initialized = false;
     client = null;
     clientid = null;
     accessToken = null;
+
     async init(clientid, accessToken) {
         if (this.initialized) { throw new Error('Trying to initialize again'); }
         this.client = await this.TwitchClient.withCredentials(clientid, accessToken);
@@ -12,21 +14,29 @@ class ClientHolder {
         this.clientid = clientid;
         this.accessToken = accessToken;
     }
+
     getClient() {
-        if (!this.initialized) { throw new Error("Client could not be initialized.") }
+        if (!this.initialized) { throw new Error("Client could not be initialized") }
         return this.client;
     }
-    restartClient(){
-        if (!this.initialized){ throw new Error("Client is already off") }
+    
+    async restartClient(){
+        this.stopClient();
+        this.startClient();
+    }
+    
+    async startClient(){
+        if(this.initialized){ throw new Error("Client is already on")}
+        if(this.clientid === null || this.accessToken === null){ throw new Error("Client must first be initialized")}
+        this.init(this.clientid, this.accessToken);
+    }
+    
+    async stopClient(){
+        if(!this.initialized){ throw new Error("Client is already stopped")}
         this.client = null;
         this.initialized = false;
-        this.init(this.clientid, this.accessToken);
-        console.log("Client Restarted");
-    }
-    stopClient(){
-        if(!this.initialized){ throw new Error("Client is already off")}
-        this.client = null;
         console.log("Client Stopped");
     }
 }
+
 module.exports = new ClientHolder();
